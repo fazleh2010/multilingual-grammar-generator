a8cdbcefe2514764a03eee343c51fe281f82ae81
Mohammad Fazleh Elahi <fazleh2010@gmail.com>
2023-06-04 11:41:22 +0200

    parser

--- src/main/java/grammar/generator/BindingConstants.java
+++
@@ -1,7 +0,0 @@
-package grammar.generator;
-
-public class BindingConstants {
-  public static final String DEFAULT_BINDING_VARIABLE = "$x";
-  public static final String BINDING_TOKEN_TEMPLATE = "(%s | %s_%s)";
-  public static final String NONE = "NONE";
-}
