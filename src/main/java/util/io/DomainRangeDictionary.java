c5b62011ee8b1ef8e80398e610b2de330dbc06bf
Mohammad Fazleh Elahi <fazleh2010@gmail.com>
2023-08-28 14:35:55 +0200

    added class lexicalizations to NounPPframe

--- src/main/java/util/io/DomainRangeDictionary.java
+++ src/main/java/util/io/DomainRangeDictionary.java
@@ -6,10 +6,12 @@
 package util.io;
 
 import java.io.File;
+import java.io.IOException;
 import static java.lang.System.exit;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.TreeMap;
 
 /**
@@ -69,17 +71,53 @@ public class DomainRangeDictionary {
         return domainOrRange;
     }
   
-    public static void main(String []args){
+    public static void main(String []args) throws IOException{
+        String[] head=new String[]{"lemon","partOfSpeech","writtenForm (singular)","writtenForm (plural)",
+                                   "preposition","SyntacticFrame","copulativeArg","prepositionalAdjunct",
+                                   "sense","reference","domain","range"};
         File domainAndRangeFileName=new File("lexicon/en/DomainOrRange.csv");
-        File outputFileName=new File("lexicon/en/outputClass.csv");
+        File outputFileName=new File("lexicon/en/nounPPframeClass.csv");
+         String classFile="lexicon/en/classes.txt";
+         Set<String> classes=FileFolderUtils.fileToSet(classFile);
         CsvFile csvFile = new CsvFile(outputFileName);
-        Map<String, List<String>> map=findDomainorRangeEnglish(domainAndRangeFileName);
-        List<String[]> rows=new ArrayList<String[]>();
-        for(String key:map.keySet()){
-            List<String> rowT = map.get(key);
-            String[] newRows=new String[]{key,"noun",rowT.get(0),rowT.get(1),"of","NounPPFrame","range","domain"};
-            System.out.println(rowT.get(0)+"noun"+rowT.get(0)+rowT.get(1)+"of"+"NounPPFrame"+"range"+"domain");
+        Map<String, List<String>> domainOrRange=findDomainorRangeEnglish(domainAndRangeFileName);
+        for(String key:classes){
+            if(domainOrRange.containsKey(key))
+               continue; 
+            List<String> rowT = new ArrayList<String>();
+            String property=key.replace("dbo:","");
+            rowT.add(property);
+            rowT.add(property+"s");
+            //System.out.println(property+" "+property+property+"s");
+            domainOrRange.put(key, rowT);
         }
+        System.out.println("size:"+domainOrRange.size());
+        
+        List<String[]> newRows=new ArrayList<String[]>();
+        newRows.add(head);
+        
+        for(String key:domainOrRange.keySet()){
+            String klass=key.replace("dbo:","");
+            List<String> rowT = domainOrRange.get(key);
+            if(key.contains("xsd:")){
+               continue;  
+            }
+            if(rowT.get(0).contains("-")){
+               continue;
+            }
+            String lexiId=rowT.get(0).toLowerCase()+"_"+"of";
+            String singular=rowT.get(0);
+            String plural=rowT.get(1);
+            String[] row=new String[]{lexiId,"noun",singular,plural,"of","NounPPFrame","range","domain",
+                                          "1","rdf:type",key,key};
+            //System.out.println(rowT.get(0)+"_"+"of"+"noun"+rowT.get(0)+rowT.get(1)+"of"+"NounPPFrame"+"range"+"domain"+
+            //                              "1"+"rdf:type"+key+key);
+            newRows.add(row);
+        }
+        csvFile.writeToCSV(newRows);
+        
+         
+        
         
     }
 }
